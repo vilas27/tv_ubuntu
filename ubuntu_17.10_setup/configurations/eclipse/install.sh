5948746ac1ee43048c9f305079055836580376d4
Tiago Vilas <vilas_33@hotmail.com>
2018-10-04 19:05:36 +0100

    Add configuration to add eclipse to start menu

---
+++ ubuntu_17.10_setup/configurations/eclipse/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo =======================================================================
+echo            Unbunt 17.10 Automatic installation of Eclipse
+echo 		        Tiago Vilas -- vilas_33@hotmail.com	
+echo =======================================================================
+
+echo =======================================================================
+echo 			       		  Installing Eclipse
+echo =======================================================================
+
+#TODO: Missing manual installation
+
+cp Eclipse.desktop /home/$USER/.local/share/applications
+
+echo =======================================================================
+echo 			   		 Finish Eclipse installation
+echo =======================================================================
